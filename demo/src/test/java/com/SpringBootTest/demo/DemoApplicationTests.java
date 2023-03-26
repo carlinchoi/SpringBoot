f54ad017c6614454c06f978bb6104426e9114ba7
YourName <TestEmail@mail.com>
2023-03-26 01:42:16 -0700

    first commit

---
+++ demo/src/test/java/com/SpringBootTest/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootTest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
